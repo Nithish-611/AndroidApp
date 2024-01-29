97c43662d43ff89b6ec57375afbe5e29136bfb98
aswin-8694 <aswin.i@zohcorp.com>
2024-01-29 23:30:50 +0530

    Date picker dialog added

--- app/src/main/java/com/example/famone/DocumentPreviewActivity.kt
+++ app/src/main/java/com/example/famone/DocumentPreviewActivity.kt
@@ -4,21 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -27,34 +24,28 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import coil.compose.AsyncImage
-import com.example.famone.data.Document
 import com.example.famone.ui.theme.FamOneTheme
+import com.example.famone.ui.theme.composables.MyDatePickerDialog
 import com.example.famone.viewmodel.DocumentViewModel
 
-class DocumentPreviewActivity : ComponentActivity(){
+class DocumentPreviewActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         viewModel = ViewModelProvider(this)[DocumentViewModel::class.java]
 
-        val documentId = intent.getIntExtra("document_id",0)
-        println("ak--> ${documentId}")
-        viewModel.getDocumentById(this,documentId)
+        val documentId = intent.getIntExtra("document_id", 0)
+        viewModel.getDocumentById(this, documentId)
 
 
 
@@ -62,20 +53,25 @@ class DocumentPreviewActivity : ComponentActivity(){
             FamOneTheme {
 
                 val document = viewModel.docList.collectAsState().value[0]
-                var image = document.imageUrl
+                val image = document.imageUrl
                 val imageList = image.split(",")
 
 
                 val pagerState = rememberPagerState()
-                val scope = rememberCoroutineScope()
-                var textState = remember{mutableStateOf(TextFieldValue(document.title?:""))}
+                val textState = remember { mutableStateOf(TextFieldValue(document.title ?: "")) }
 
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = MaterialTheme.colorScheme.background),
+                val isDateTimePickerVisible = remember { mutableStateOf(false) }
+                val date = remember {
+                    mutableStateOf("Set reminder")
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.background),
                     contentAlignment = Alignment.TopCenter,
 
-                )
+                    )
 
                 {
 
@@ -83,22 +79,26 @@ class DocumentPreviewActivity : ComponentActivity(){
                         HorizontalPager(
                             pageCount = imageList.size,
                             state = pagerState,
-                            key = {imageList[it]},
+                            key = { imageList[it] },
                             pageSize = PageSize.Fill,
                             modifier = Modifier
                                 .padding(start = 16.dp, end = 16.dp, top = 100.dp)
                                 .fillMaxWidth()
                                 .height(360.dp)
-                        ) {index->
-                            AsyncImage(modifier = Modifier.fillMaxSize(), model = imageList[index], contentDescription = "123")
+                        ) { index ->
+                            AsyncImage(
+                                modifier = Modifier.fillMaxSize(),
+                                model = imageList[index],
+                                contentDescription = "123"
+                            )
                         }
 
                         TextField(
                             value = textState.value,
                             onValueChange = {
-                                textState.value=it
+                                textState.value = it
                             },
-                            label = { Text(text="Document Title")},
+                            label = { Text(text = "Document Title") },
                             modifier = Modifier
                                 .padding(top = 16.dp, start = 16.dp, end = 16.dp)
                                 .fillMaxWidth(),
@@ -107,18 +107,35 @@ class DocumentPreviewActivity : ComponentActivity(){
                             ),
                             colors = TextFieldDefaults.textFieldColors(
                                 containerColor = Color.LightGray,
-                                textColor = Color.Black,
+                                focusedTextColor = Color.Black,
+                                unfocusedTextColor = Color.Black,
                                 focusedLabelColor = Color.DarkGray,
                                 unfocusedLabelColor = Color.Black,
                                 focusedIndicatorColor = Color.Black
                             )
                         )
+                        Box(contentAlignment = Alignment.Center) {
+                            Button(
+                                modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp),
+                                onClick = { isDateTimePickerVisible.value = true },
+                                colors = ButtonDefaults.buttonColors(containerColor = Color.LightGray))
+                            {
+                                Text(text = date.value, fontSize = 24.sp)
+                            }
+                        }
 
+                        if (isDateTimePickerVisible.value) {
+                            MyDatePickerDialog(
+                                onDateSelected = {
+                                    if(it.isNotEmpty()) {
+                                        date.value = it
+                                    }
+                                },
+                                onDismiss = { isDateTimePickerVisible.value = false }
+                            )
+                        }
                     }
-
                 }
-
-
             }
         }
     }
---
+++ app/src/main/java/com/example/famone/ui/theme/composables/DateTimePickerDialog.kt
@@ -0,0 +1,53 @@
+package com.example.famone.ui.theme.composables
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import java.text.SimpleDateFormat
+import java.util.Date
+
+@SuppressLint("SimpleDateFormat")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyDatePickerDialog(
+    onDateSelected: (String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    val selectedDate = datePickerState.selectedDateMillis?.let {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val date = Date(it)
+        dateFormat.format(date)
+    } ?: ""
+
+    DatePickerDialog(
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+            Button(onClick = {
+                onDateSelected(selectedDate)
+                onDismiss()
+            }
+
+            ) {
+                Text(text = "OK")
+            }
+        },
+        dismissButton = {
+            Button(onClick = {
+                onDismiss()
+            }) {
+                Text(text = "Cancel")
+            }
+        }
+    ) {
+        DatePicker(
+            state = datePickerState
+        )
+    }
+}
